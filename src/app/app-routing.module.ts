0568285ecd3c51822f996755ae9e86b6d060b8ae
andrewli <andrew.li@infodatinc.com>
2022-06-16 16:27:04 -0500

    working on signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'admin', pathMatch: 'full' }
+  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: 'signup', loadChildren: () => import('./auth-pages/signup/signup.module').then(m => m.SignupModule) },
+  { path: 'login', loadChildren: () => import('./auth-pages/login/login.module').then(m => m.LoginModule) },
+  { path: '**', redirectTo: '' }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth-pages/login/login.component';
+import { SignupComponent } from './auth-pages/signup/signup.component';
+import { LoginModule } from './auth-pages/login/login.module';
 
 @NgModule({
   declarations: [
