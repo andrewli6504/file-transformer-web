1ffe834caca732b1946f24517f2d51bec24a0e9f
andrewli <andrew.li@infodatinc.com>
2022-06-16 16:27:04 -0500

    working on signup page

--- src/app/auth-pages/signup/signup.module.ts
+++ src/app/auth-pages/signup/signup.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
@@ -11,6 +17,13 @@ import { SignupComponent } from './signup.component';
   ],
   imports: [
     CommonModule,
+    SignupRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
     SignupRoutingModule
   ]
 })
