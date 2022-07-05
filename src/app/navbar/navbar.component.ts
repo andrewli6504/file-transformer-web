a1a52190972468f96747ab68b28948e5483f95f9
andrewli <andrew.li@infodatinc.com>
2022-07-05 15:20:51 -0500

    project card

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,19 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
 
   loggedIn: boolean;
-  mobile: boolean;
   showCard: {};
 
   constructor() { }
 
   ngOnInit(): void {
     this.loggedIn = false;
-    this.mobile = false;
     this.clearDropdowns();
   }
 
