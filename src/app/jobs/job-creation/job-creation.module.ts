4e3d27479fe3379a45891120071625d634f77c17
andrewli <andrew.li@infodatinc.com>
2022-07-25 13:52:18 -0500

    removed unused job progress

--- src/app/jobs/job-creation/job-creation.module.ts
+++ src/app/jobs/job-creation/job-creation.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 
 import { JobCreationRoutingModule } from './job-creation-routing.module';
 import { JobCreationComponent } from './job-creation.component';
-import { JobProgressComponent } from './job-progress/job-progress.component';
 import { JobConnectionComponent } from './job-steps/job-connection/job-connection.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -24,7 +23,6 @@ import { ComponentModule } from 'src/app/common/component.module';
 @NgModule({
   declarations: [
     JobCreationComponent,
-    JobProgressComponent,
     JobConnectionComponent,
     JobSourceComponent,
     JobDestinationComponent,
