e96862abb2e31119183ceddbffcaaa7c99aad156
andrewli <andrew.li@infodatinc.com>
2022-08-01 11:10:35 -0500

    desktop project navbar

--- src/app/projects/project-navbar/project-navbar.component.ts
+++ src/app/projects/project-navbar/project-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-project-navbar',
@@ -10,6 +11,13 @@ export class ProjectNavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+    popoverList.forEach(element => {
+      console.log(element);
+    });
   }
 
 }
