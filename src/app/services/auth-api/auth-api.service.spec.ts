0f77a320ffde3ad9b9d1c08e91c6a4de0e68f1bb
andrewli <andrew.li@infodatinc.com>
2022-08-09 11:20:02 -0500

    work on tests

--- src/app/services/auth-api/auth-api.service.spec.ts
+++ src/app/services/auth-api/auth-api.service.spec.ts
@@ -10,10 +10,72 @@ describe('AuthApiService', () => {
     TestBed.configureTestingModule({
       providers: [HttpClient, HttpHandler]
     });
+
+
+    //Mocking local storage for testing
+    let store = {};
+    const mockLocalStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        delete store[key];
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+    spyOn(localStorage, 'getItem')
+      .and.callFake(mockLocalStorage.getItem);
+    spyOn(localStorage, 'setItem')
+      .and.callFake(mockLocalStorage.setItem);
+    spyOn(localStorage, 'removeItem')
+      .and.callFake(mockLocalStorage.removeItem);
+    spyOn(localStorage, 'clear')
+      .and.callFake(mockLocalStorage.clear);
+
     service = TestBed.inject(AuthApiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have storage access', () => {
+    expect(service.hasStorageAccess).toBeTrue();
+  })
+
+  it('should set session', () => {
+    let authResponse = {
+      "accessToken": "accessToken",
+      "email": "email",
+      "firstName": "firstName",
+      "lastName": "lastName",
+      "username": "username",
+      "id": "id"
+    };
+
+    service.setSession(authResponse);
+
+    expect(localStorage.getItem("token")).toEqual("accessToken");
+    expect(localStorage.getItem("email")).toEqual("email");
+    expect(localStorage.getItem("firstname")).toEqual("firstName");
+    expect(localStorage.getItem("lastname")).toEqual("lastName");
+    expect(localStorage.getItem("username")).toEqual("username");
+    expect(localStorage.getItem("userID")).toEqual("id");
+  });
+
+  it('should clear session', () => {
+    localStorage.setItem("token", "token");
+    service.clearSession();
+
+    expect(localStorage.getItem("token")).toBeNull();
+  });
+
+  // it('should create user', () => {
+
+  // });
 });
--- src/app/services/rest-api-base/rest-api-base.service.spec.ts
+++ src/app/services/rest-api-base/rest-api-base.service.spec.ts
@@ -10,10 +10,44 @@ describe('RestApiBaseService', () => {
     TestBed.configureTestingModule({
       providers: [HttpClient, HttpHandler]
     });
+
+    //Mocking local storage for testing
+    let store = {};
+    const mockLocalStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        delete store[key];
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+    spyOn(localStorage, 'getItem')
+      .and.callFake(mockLocalStorage.getItem);
+    spyOn(localStorage, 'setItem')
+      .and.callFake(mockLocalStorage.setItem);
+    spyOn(localStorage, 'removeItem')
+      .and.callFake(mockLocalStorage.removeItem);
+    spyOn(localStorage, 'clear')
+      .and.callFake(mockLocalStorage.clear);
+      
+    localStorage.setItem("token", "token");
+
     service = TestBed.inject(RestApiBaseService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get headers', () => {
+    expect(service.getHeaders().get("Authorization")).toEqual("token");
+  });
+
+  // it('should ')
 });
